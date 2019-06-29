8f48c227952430c06b99928d456aefb0224c3c65
sinaill <980195840@qq.com>
2019-06-29 14:20:39 +0800

    Create fuqiang.sh
    创建shadowsocks脚本

---
+++ fuqiang.sh
@@ -0,0 +1,48 @@
+CONFIG_FILE=/etc/shadowsocks.json
+SERVICE_FILE=/etc/systemd/system/shadowsocks.service
+
+# install shadowsocks
+pip install --upgrade pip
+pip install shadowsocks
+
+# create shadowsocls config
+cat <<EOF | sudo tee ${CONFIG_FILE}
+{
+  "server": "0.0.0.0",
+  "local_address": "127.0.0.1",
+  "local_port": 1080,
+  "port_password": {
+    "8388": "cairuijie",
+    "8389": "zhongwenjiao",
+    "8390": "gongyong1",
+    "8391": "chenyujuan",
+    "8392": "chanchanchan",
+    "8393": "wenziqi"
+  },
+  "timeout": 600,
+  "method": "aes-256-cfb"
+}
+EOF
+
+# create service
+cat <<EOF | sudo tee ${SERVICE_FILE}
+[Unit]
+Description=Shadowsocks
+
+[Service]
+TimeoutStartSec=0
+ExecStart=/usr/bin/ssserver -c ${CONFIG_FILE}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+systemctl enable shadowsocks
+systemctl start shadowsocks
+
+# view service status
+sleep 5
+systemctl status shadowsocks -l
+
+cat /etc/shadowsocks.json
